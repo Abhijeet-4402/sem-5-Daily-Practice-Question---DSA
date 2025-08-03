46f0ecd3099c9a4859f0f44a256038be503e8f20
Abhijeet-4402 <abhirajpoot4402@gmail.com>
2025-08-04 00:01:50 +0530

    Create 2006-FindTheStudentThatWillReplaceTheChalk

---
+++ 2006-FindTheStudentThatWillReplaceTheChalk/2006-FindTheStudentThatWillReplaceTheChalk.java
@@ -0,0 +1,19 @@
+// Last updated: 4/8/2025, 12:01:50 am
+class Solution {
+    public int chalkReplacer(int[] chalk, int k) {
+        long total_chalk=0l;
+        for(int chalkNeeded:chalk){
+            total_chalk+=chalkNeeded;
+        }
+        long chalk_remaning=k;
+        chalk_remaning%=total_chalk;
+        for(int i = 0; i<chalk.length; i++){
+            long chalkNeed = chalk[i];
+            if(chalk_remaning<chalkNeed){
+                return i;
+            }
+            chalk_remaning-=chalkNeed;
+        }
+        return -1;
+    }
+}
