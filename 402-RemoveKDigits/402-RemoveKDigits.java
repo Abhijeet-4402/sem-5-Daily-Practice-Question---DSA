43dbe1340bdde18431ad0ad6e7bf6b1d67e76a4a
Abhijeet-4402 <abhirajpoot4402@gmail.com>
2025-08-04 00:05:41 +0530

    Create 402-RemoveKDigits

---
+++ 402-RemoveKDigits/402-RemoveKDigits.java
@@ -0,0 +1,26 @@
+// Last updated: 4/8/2025, 12:05:40 am
+class Solution {
+    public String removeKdigits(String num, int k) {
+        Stack<Character> st = new Stack<>();
+        for (char c : num.toCharArray()) {
+            while (k > 0 && !st.isEmpty() && st.peek() > c) {
+                st.pop();
+                k--;
+            }
+            st.push(c);
+        }
+        while (k > 0 && !st.isEmpty()) {
+            st.pop();
+            k--;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (char c : st) {
+            sb.append(c);
+        }
+        while (sb.length() > 0 && sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
+        }
+
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}
