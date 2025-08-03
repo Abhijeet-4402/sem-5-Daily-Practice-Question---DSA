b0ea7931f8de20879ebdcc46ad646815db3f1158
Abhijeet-4402 <abhirajpoot4402@gmail.com>
2025-08-04 00:01:13 +0530

    Create 3201-DistributeCandiesAmongChildrenIi

---
+++ 3201-DistributeCandiesAmongChildrenIi/3201-DistributeCandiesAmongChildrenIi.java
@@ -0,0 +1,10 @@
+// Last updated: 4/8/2025, 12:01:13 am
+class Solution {
+    public long distributeCandies(int n, int limit) {
+        return CO(n)-(3*CO(n-(limit+1)))+(3*CO(n-2*(limit+1)))-(CO(n-3*(limit+1)));
+    }
+    public long CO(long n){
+        if(n<0) return 0;
+        return ((n+2)*(n+1))/2;
+    }
+}
