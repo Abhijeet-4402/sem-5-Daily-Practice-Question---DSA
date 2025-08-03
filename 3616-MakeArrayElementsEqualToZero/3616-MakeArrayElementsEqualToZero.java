09184494a5996c7e36cd3dee8f58eb93db133f09
Abhijeet-4402 <abhirajpoot4402@gmail.com>
2025-08-04 00:01:01 +0530

    Create 3616-MakeArrayElementsEqualToZero

---
+++ 3616-MakeArrayElementsEqualToZero/3616-MakeArrayElementsEqualToZero.java
@@ -0,0 +1,22 @@
+// Last updated: 4/8/2025, 12:01:00 am
+class Solution {
+    public int countValidSelections(int[] nums) {
+        int count = 0;
+        int sum = 0;
+        int csum =0;
+        for(int i : nums){
+            sum+=i;
+        }
+        for(int i : nums){
+            csum+=i;
+            if(i==0){
+                if(2*csum-sum == 0){
+                    count+=2;
+                }else if(2*csum-sum == 1 || 2*csum-sum == -1 ){
+                    count+=1;
+                }
+            }
+        }
+        return count;
+    }
+}
