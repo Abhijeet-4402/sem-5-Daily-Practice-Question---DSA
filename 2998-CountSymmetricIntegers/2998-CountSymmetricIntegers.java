a9b69e6b0c9648625ce0567479a979447c9a1e4e
Abhijeet-4402 <abhirajpoot4402@gmail.com>
2025-08-04 00:01:20 +0530

    Create 2998-CountSymmetricIntegers

---
+++ 2998-CountSymmetricIntegers/2998-CountSymmetricIntegers.java
@@ -0,0 +1,26 @@
+// Last updated: 4/8/2025, 12:01:20 am
+class Solution {
+    public int countSymmetricIntegers(int low, int high) {
+        int count = 0;
+
+        for (int i = low; i <= high; i++) {
+            String s = String.valueOf(i);
+            int len = s.length();
+
+            if (len % 2 != 0) continue; 
+            int half = len / 2;
+            int leftSum = 0, rightSum = 0;
+
+            for (int j = 0; j < half; j++) {
+                leftSum += s.charAt(j) - '0';
+                rightSum += s.charAt(j + half) - '0';
+            }
+
+            if (leftSum == rightSum) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
