e37bd30a65ea0774d0a1c5adb2bff5127e4d370d
Abhijeet-4402 <abhirajpoot4402@gmail.com>
2025-08-04 00:03:25 +0530

    Create 986-LargestTimeForGivenDigits

---
+++ 986-LargestTimeForGivenDigits/986-LargestTimeForGivenDigits.java
@@ -0,0 +1,20 @@
+// Last updated: 4/8/2025, 12:03:25 am
+class Solution {
+    public String largestTimeFromDigits(int[] arr) {
+        String ans = "";
+        for(int i = 0; i <4 ; i++){
+            for(int j = 0; j<4 ;j++){
+                for(int k = 0 ; k<4;k++){
+                    if(i==j || i==k || j==k)continue;
+                    String hour = ""+arr[i]+arr[j];
+                    String minute = ""+arr[k]+arr[6-i-j-k];
+                    String time = hour+":"+minute;
+                    if(hour.compareTo("24") < 0 && minute.compareTo("60") < 0 && ans.compareTo(time) < 0)ans = time;
+
+                }
+                
+            }
+        }
+        return ans;
+    }
+}
