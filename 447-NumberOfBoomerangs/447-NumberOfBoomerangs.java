7dc046970120803fa5832714179cd89ec7121b1a
Abhijeet-4402 <abhirajpoot4402@gmail.com>
2025-08-04 00:05:21 +0530

    Create 447-NumberOfBoomerangs

---
+++ 447-NumberOfBoomerangs/447-NumberOfBoomerangs.java
@@ -0,0 +1,20 @@
+// Last updated: 4/8/2025, 12:05:20 am
+class Solution {
+    public int numberOfBoomerangs(int[][] points) {
+        int ans = 0;
+        for(int[] p : points){
+            Map<Integer,Integer> dist = new HashMap<>();
+            for(int [] q : points){
+                int dx = p[0]-q[0];
+                int dy = p[1]-q[1];
+                int sqdist = dx*dx+dy*dy;
+
+                dist.put(sqdist,dist.getOrDefault(sqdist,0)+1);
+            }
+            for(int i : dist.values()){
+                ans+=i*(i-1);
+            }
+        }
+        return ans;
+    }
+}
