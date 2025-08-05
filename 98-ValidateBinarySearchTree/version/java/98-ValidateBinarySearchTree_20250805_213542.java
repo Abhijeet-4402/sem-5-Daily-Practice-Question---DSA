28b7ef487709f1fe158f14afacf23a1d5c7ca11a
Abhijeet-4402 <abhirajpoot4402@gmail.com>
2025-08-05 21:35:42 +0530

    Create 98-ValidateBinarySearchTree

---
+++ 98-ValidateBinarySearchTree/version/java/98-ValidateBinarySearchTree_20250805_213542.java
@@ -0,0 +1,42 @@
+// Last updated: 5/8/2025, 9:35:42 pm
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return ValidBst(root).isbst;
+    }
+
+    public BstPair ValidBst(TreeNode root) {
+        if (root == null) return new BstPair();
+
+        BstPair lbp = ValidBst(root.left);
+        BstPair rbp = ValidBst(root.right);
+
+        BstPair sbp = new BstPair();  // Fixed this line
+
+        sbp.min = Math.min(root.val, Math.min(lbp.min, rbp.min));
+        sbp.max = Math.max(root.val, Math.max(lbp.max, rbp.max));
+        sbp.isbst = lbp.isbst && rbp.isbst && (lbp.max < root.val) && (rbp.min > root.val);
+
+        return sbp;
+    }
+
+    class BstPair {
+        long min = Long.MAX_VALUE;
+        long max = Long.MIN_VALUE;
+        boolean isbst = true;
+    }
+}
