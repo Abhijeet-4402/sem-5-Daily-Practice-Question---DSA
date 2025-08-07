e7adf7a283ee2d95221f632e1eee36bd907e7a61
Abhijeet-4402 <abhirajpoot4402@gmail.com>
2025-08-07 11:26:19 +0530

    Create 450-DeleteNodeinaBST

---
+++ 450-DeleteNodeinaBST/version/java/450-DeleteNodeinaBST_20250807_112618.java
@@ -0,0 +1,59 @@
+// Last updated: 7/8/2025, 11:26:18 am
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val < key) {
+            root.right = deleteNode(root.right, key);
+        } else if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else {
+            if (root.left == null) return root.right;
+            else if (root.right == null) return root.left;
+            else {
+                // int min = min(root.right);
+                // root.right = deleteNode(root.right, min);
+                // root.val = min;
+
+                int max = max(root.left);
+                root.left = deleteNode(root.left, max);
+                root.val = max;
+            }
+        }
+
+        return root;
+    }
+
+    public int min(TreeNode root) {
+        if (root == null) {
+            return Integer.MAX_VALUE;
+        }
+        int l = min(root.left);
+        return Math.min(l, root.val);
+    }
+
+    public int max(TreeNode root) {
+        if (root == null) {
+            return Integer.MIN_VALUE;
+        }
+        int l = max(root.right);
+        return Math.max(l, root.val);
+    }
+}
