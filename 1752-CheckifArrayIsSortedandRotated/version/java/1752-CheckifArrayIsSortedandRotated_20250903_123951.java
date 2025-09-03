c03498a8cc0db0e6d185b8e59ddbb902f90a30ef
Abhijeet-4402 <abhirajpoot4402@gmail.com>
2025-09-03 12:39:52 +0530

    Create 1752-CheckifArrayIsSortedandRotated

---
+++ 1752-CheckifArrayIsSortedandRotated/version/java/1752-CheckifArrayIsSortedandRotated_20250903_123951.java
@@ -0,0 +1,12 @@
+// Last updated: 3/9/2025, 12:39:51 pm
+class Solution {
+    public boolean check(int[] nums) {
+        int count=0;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i]<nums[i-1]) count++;
+        }
+        if(count==0) return true;
+        if(count==1 && nums[0]>=nums[nums.length-1]) return true;
+        return false;
+    }
+}
