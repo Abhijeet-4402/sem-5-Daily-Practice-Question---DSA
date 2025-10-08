bd6d23d43204c7d822b039e5cfe255bae5897d1f
Abhijeet-4402 <abhirajpoot4402@gmail.com>
2025-10-09 00:21:19 +0530

    Create 1025-DivisorGame

---
+++ 1025-DivisorGame/version/java/1025-DivisorGame_20251009_002118.java
@@ -0,0 +1,22 @@
+// Last updated: 9/10/2025, 12:21:18 am
+class Solution {
+    public boolean divisorGame(int n) {
+        int[] dp = new int[n+1];
+		Arrays.fill(dp, 5);
+		int x = OP(n, dp);
+		return x==-1?true:false;
+	}
+
+	private static int OP(int n, int[] dp) {
+		// TODO Auto-generated method stub
+		if(n==1 || n==-1) return 0;
+		if(n==0)return -1;
+		if(dp[n]!=5)return dp[n];
+		int a = 0;
+		for(int i =1;i<n;i++) {
+			if(n%i==0) a=Math.min(a, OP(OP(n-i,dp),dp));
+		}
+		return dp[n]= a;
+	}
+    
+}
