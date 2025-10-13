3ca4ee08e96b64eef6d1e2b8abc7395301e1ec82
Abhijeet Singh <abhirajpoot4402@gmail.com>
2025-10-14 01:41:02 +0530

    Create 646-MaximumLengthofPairChain

---
+++ 646-MaximumLengthofPairChain/version/java/646-MaximumLengthofPairChain_20251014_014101.java
@@ -0,0 +1,17 @@
+// Last updated: 14/10/2025, 1:41:01 am
+class Solution {
+    public int findLongestChain(int[][] pairs) {
+        // Sort pairs in ascending order based on the second element.
+        Arrays.sort(pairs, (a, b) -> a[1] - b[1]);
+        int curr = Integer.MIN_VALUE;
+        int ans = 0;
+
+        for (int[] pair : pairs) {
+            if (pair[0] > curr) {
+                ans++;
+                curr = pair[1];
+            }
+        }
+        return ans;
+    }
+}
