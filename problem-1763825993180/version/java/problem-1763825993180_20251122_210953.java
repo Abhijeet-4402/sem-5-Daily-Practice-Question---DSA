b348b238753341a88777c24097b8a8c143790088
Abhijeet Singh <abhirajpoot4402@gmail.com>
2025-11-22 21:09:53 +0530

    Create

---
+++ problem-1763825993180/version/java/problem-1763825993180_20251122_210953.java
@@ -0,0 +1,22 @@
+// Last updated: 22/11/2025, 9:09:53 pm
+class Solution {
+    public int[] lexSmallestNegatedPerm(int n, long target) {
+        long mx = (long) n*(n+1)/2;
+        if(target>mx || target<-mx) return new int[0];
+        int[] a = new int[n];
+        for(int i =0;i<n;i++)a[i] = i+1;
+        long d = mx-target;
+        if((d&1)==1) return new int[0];
+        d>>=1;
+
+        for(int i = n-1;i>=0&& d>0;i--){
+            if(a[i]<=d){
+                d-=a[i];
+                a[i]=-a[i];
+            }
+        }
+
+        Arrays.sort(a);
+        return a;
+    }
+}
