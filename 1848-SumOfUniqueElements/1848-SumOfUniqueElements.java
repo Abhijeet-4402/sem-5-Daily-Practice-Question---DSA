d446d2698e9788d295441d624ddb0da358a7b435
Abhijeet Singh <abhirajpoot4402@gmail.com>
2026-01-21 11:44:03 +0530

    Create 1848-SumOfUniqueElements

---
+++ 1848-SumOfUniqueElements/1848-SumOfUniqueElements.java
@@ -0,0 +1,16 @@
+// Last updated: 21/1/2026, 11:44:02 am
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i: nums){
+            map.put(i, map.getOrDefault(i,0)+1);
+        }
+        int sum = 0;
+        for(int i:map.keySet()){
+            if(map.get(i)==1){
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+}
