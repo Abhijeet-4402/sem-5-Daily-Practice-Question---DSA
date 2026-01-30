6cff1d7b73ed7ae77575e0383cf393760d8afa6b
Abhijeet Singh <abhirajpoot4402@gmail.com>
2026-01-30 13:10:17 +0530

    Create 1823-FindtheWinneroftheCircularGame

---
+++ 1823-FindtheWinneroftheCircularGame/version/java/1823-FindtheWinneroftheCircularGame_20260130_131016.java
@@ -0,0 +1,14 @@
+// Last updated: 30/1/2026, 1:10:16 pm
+1class Solution {
+2    public int findTheWinner(int n, int k) {
+3        return solve(n,k)+1;
+4
+5    }
+6    public static int solve(int n,int k){
+7        if(n==1){
+8            return 0;
+9        }
+10        int a=solve(n-1,k);
+11        return (a+k)%n;
+12    }
+13}
